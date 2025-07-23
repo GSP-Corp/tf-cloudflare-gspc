fd5d621bc07613f58badf1bae90f2627d52de2fe
serundeputy <serundeputy@gmail.com>
2025-07-23 13:37:06 -0400

    Adding s3 remote state.

--- versions.tf
+++ versions.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5"
     }
   }
+  backend "s3" {
+    bucket       = "tf-gspc-cloudflare-state"
+    key          = "cloudflare/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
 
 provider "cloudflare" {}
